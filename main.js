d4cda69b65110ed13d476e185aad1e94bc5752b9
scanton <satori.canton@lifeplus.com>
2018-03-16 15:28:43 -0500

    added icon files

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
+var path = require('path');
+
 var mainWindow = null;
 
 app.on('ready', () => {
-	mainWindow = new BrowserWindow({ width: 1200, height: 1000, center: true, frame: false });
+	mainWindow = new BrowserWindow({ width: 1200, height: 1000, center: true, frame: false, icon: path.join(__dirname, 'assets/icons/png/64x64.png') });
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
 	mainWindow.webContents.openDevTools();
 });
