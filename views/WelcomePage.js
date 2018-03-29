f574eb6a58016fc9193d046b20e53f26059d2dba
Satori Canton <satoricanton@gmail.com>
2018-03-28 21:55:22 -0500

    worked on welcome page and theme

--- views/WelcomePage.js
+++ views/WelcomePage.js
@@ -15,6 +15,14 @@
 					<div class="info-panel">
 						<h2>Getting Started</h2>
 						<hr />
+						<h3>Start By Creating a Connection</h3>
+						<p>Click the <span class="connections-tab-example"><span class="glyphicon glyphicon-globe"></span> Connections Tab</span> on the tool bar to the left to create and organize your connections.</p>
+						<hr />
+						<h3>Understanding Projects</h3>
+						<p>Projects are a way of organizing your LIVE connections.</p>
+						<p>Connection data (the information about how to connect to a particular data source) is shared between projects.  You only have to create a connection once, and you will be able to use it in any project.</p>
+						<p>You CAN have LIVE connections to the SAME data source in more than one project at the SAME TIME.</p>
+						<hr />
 						<h3>Documentation</h3>
 						<p>Documentation will be produced and available at <a target="_blank" href="http://connectopus.org">Connectopus.org</a></p>
 					</div>
