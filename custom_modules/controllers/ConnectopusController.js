37e174f2c09d32a47bacb148415243fc92a4a897
scanton <satori.canton@lifeplus.com>
2018-05-10 10:01:32 -0500

    working on config import

--- custom_modules/controllers/ConnectopusController.js
+++ custom_modules/controllers/ConnectopusController.js
@@ -458,7 +458,7 @@ module.exports = class ConnectopusController extends EventEmitter {
 								}
 							}
 						}
-						this._fs.writeJsonSync(__dirname.split("custom_modules/controllers")[0] + "working_files/config.json", conf);
+						this._fs.outputJsonSync(__dirname.split("custom_modules/controllers")[0] + "working_files/config.json", conf, { spaces: '\t' });
 						this.configModel.loadConfig();
 						this.showConnectionsPage();
 					}
